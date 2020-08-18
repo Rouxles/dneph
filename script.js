470d12b4a8819bb1e7c15445c7ecf9ccca06deee
Antonio Kam <19kamh3@gmail.com>
2020-08-18 17:19:26 +0800

    more updates to script

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 var averageString = ""
 
 function readAverage() {
+    let separators = [" +","="]
     let string = document.getElementById("average").value;
-    console.log(string);
+    let times = string.split("Time List:");
+    times = times[times.length-1]
+    elements = times.replace(/[\(\)\+\,(\n)]+/g,'').split(new RegExp(separators.join("|"),'g'))
+    times = elements.filter(element => (!isNaN(element) && element.slice(-1) !== ".") || element.includes("DNF"))
+    console.log(times);
 }
 
 function hi() {
