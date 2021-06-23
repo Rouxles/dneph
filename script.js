be37abad6be593eaa0055301d7ecd413e4a2ae55
Antonio Kam <19kamh3@gmail.com>
2021-06-23 23:01:24 +0800

    add best weighted mean + something special :eyes:
    :eyes: :eyes: :eyes:

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let average_values = [5, 12, 25, 50, 100, 1000];
-let ids = ["global", "weighted", "best"];
+let ids = ["global", "weighted", "best", "best_weighted"];
 
 function readAverage() {
     let separators = [" +", "="]
@@ -56,31 +56,26 @@ function average(times, magnitude) {
         let no_dnf = arr.filter((e) => {
             return !DNFCheck(e)
         })
+        let dnf = arr.filter((e) => DNFCheck(e))
         let bounds = Math.ceil(0.05 * len);
-        let dnf_count = len-no_dnf.length;
-        let final_length = len-(bounds);
+        let final_array;
         if (no_dnf.length >= len - bounds) {
             no_dnf.sort(function (a, b) {
                 return Number(a) - Number(b)
             });
 
-            for(let i=0; i<dnf_count; i++) {
-                no_dnf.shift();
-            }
+            final_array = no_dnf.concat(dnf)
 
-            while(no_dnf.length > final_length) {
-                no_dnf.shift();
-                no_dnf.pop();
-            }
+            let return_array = final_array.slice(bounds,len-bounds)
 
-            return (no_dnf.reduce((a, b) => Number(a) + Number(b)) / no_dnf.length);
+            return (return_array.reduce((a, b) => Number(a) + Number(b)) / return_array.length);
         } else {
             return "DNF"
         }
     }
     let best = "DNF";
     let current;
-    for (let i = 0; i < times.length - magnitude; i++) {
+    for (let i = 0; i <= times.length - magnitude; i++) {
         current = aox(times.slice(i, i + magnitude));
         if (current < best || best == "DNF") {
             best = current;
@@ -177,14 +172,145 @@ function stats(type, time, id) {
 }
 
 function clearStats(ids) {
-    let i;
     ids.forEach(i => {
         document.getElementById(`${i}_container`).innerHTML = `<ul id="${i}_stats"></ul>`;
     })
 }
 
+function bestWeightedmo100(times) {
+    let best;
+    let best_index;
+    let time_list;
+    for (let i = 0; i <= times.length - 100; i++) {
+        time_list = times.slice(i, i+100)
+        no_dnf = time_list.filter((e) => {
+            return !DNFCheck(e)
+        })
+        no_dnf.sort(function (a, b) {
+            return Number(a) - Number(b)
+        })
+        let sum = (arr) => {
+            return arr.reduce((a, b) => Number(a) + Number(b));
+        }
+        half_length = 50
+        if (no_dnf.length >= 50) {
+            while (no_dnf.length > 50) {
+                no_dnf.pop();
+            }
+            if(!best || Math.round(sum(no_dnf) * 100 / no_dnf.length) / 100 < best) {
+                best = Math.round(sum(no_dnf) * 100 / no_dnf.length) / 100
+                best_index = i+1
+            }
+        }
+    }
+    stats(`Best Weighted Mean of 100`, best, "best_weighted_stats");
+    stats(`Begins on Solve Number`, best_index, "best_weighted_stats");
+    let graphData = times.slice(best_index, best_index+100);
+    let noDNFGraphData = graphData.filter((e) => {
+        return !DNFCheck(e)
+    })
+    drawGraph(noDNFGraphData)
+}
+
+function drawGraph(data) {
+
+    let d = document.getElementById("chartContainer");
+    let h = document.createElement("h3");
+    let t = document.createTextNode("Solve Distribution:");
+    d.appendChild(document.createElement("BR"));
+    h.appendChild(t);
+    d.appendChild(h);
+
+    console.log(data);
+    // set the dimensions and margins of the graph
+    var margin = {
+        top: 30,
+        right: 30,
+        bottom: 30,
+        left: 50
+      },
+      width = 400 - margin.left - margin.right,
+      height = 350 - margin.top - margin.bottom;
+
+    // append the svg object to the body of the page
+    var svg = d3.select("#chartContainer")
+      .append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+      .append("g")
+      .attr("transform",
+        "translate(" + margin.left + "," + margin.top + ")");
+
+        // add the x Axis
+        var x = d3.scaleLinear()
+          .domain([Math.min(...data)-1, Math.max(...data)+1])
+          .range([0, width]);
+        svg.append("g")
+          .attr("transform", "translate(0," + height + ")")
+          .call(d3.axisBottom(x));
+      
+        // add the y Axis
+        var y = d3.scaleLinear()
+          .range([height, 0])
+          .domain([0, 0.3]);
+        // svg.append("g")
+        //   .call(d3.axisLeft(y));
+
+        console.log(stdev(data))
+
+        // Compute kernel density estimation
+        var kde = kernelDensityEstimator(kernelEpanechnikov(stdev(data)), x.ticks(20))
+        var density = kde(data)
+      
+        // Plot the area
+        svg.append("path")
+          .attr("class", "mypath")
+          .datum(density)
+          .attr("fill", "#007bff")
+          .attr("opacity", "1")
+          .attr("stroke", "#000")
+          .attr("stroke-width", 2)
+          .attr("stroke-linejoin", "round")
+          .attr("d", d3.area()
+            .curve(d3.curveBasis)
+            .x(function (d) { return x(d[0]); })
+            .y1(function (d) { return y(d[1]); })
+            .y0(y(0))
+          );
+}
+
+function kernelDensityEstimator(kernel, X) {
+    return function (V) {
+      return X.map(function (x) {
+        return [x, d3.mean(V, function (v) {
+          return kernel(x - v);
+        })];
+      });
+    };
+  }
+
+  function kernelEpanechnikov(k) {
+    return function (v) {
+      return Math.abs(v /= k) <= 1 ? 0.75 * (1 - v * v) / k : 0;
+    };
+  }
+
+function stdev(array) {
+    let sum = (arr) => {
+        return arr.reduce((a, b) => Number(a) + Number(b));
+    }
+    const n = array.length
+    const mean = sum(array) / n
+    return Math.sqrt(array.map(x => Math.pow(x - mean, 2)).reduce((a, b) => a + b) / n)
+}
+
+function clearGraph() {
+    document.getElementById("chartContainer").innerHTML = "";
+}
+
 function main() {
     clearStats(ids);
+    clearGraph();
     let time_list = readAverage();
     let length = time_list.length
     if (length > 1) {
@@ -200,6 +326,9 @@ function main() {
                 average(time_list, a);
             }
         })
+        if (length >= 100) {
+            bestWeightedmo100(time_list)
+        }
     } else if (length == 1 && !isNaN(time_list[0])) {
         gmean(time_list);
         stats("Accuracy", `${Math.floor((NotDNFCount(time_list)*10000/length))/100}%`, "global_stats")
